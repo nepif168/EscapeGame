116f221a9e1de202e6f22e68e86888e4e1c951b5
Icchan <nepif168@gmail.com>
2017-09-20 15:57:43 +0900

    PlayerAnimationのStartの最後の行で謎のクラッシュ発生、Unityに問い合わせ中

---
+++ EscapeGame/Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class PlayerAnimation : MonoBehaviour {
+
+    PlayerCore playerCore;
+    IInputProvider playerInput;
+    Animator animator;
+
+    private void Awake()
+    {
+        playerCore = GetComponent<PlayerCore>();
+        playerInput = GetComponent<IInputProvider>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        playerInput.IsJump.Where(j => j).Subscribe(_ => animator.speed = 3);
+        playerInput.IsJump.Where(j => !j).Subscribe(_ => animator.speed = 1);
+        playerCore.IsDead.Where(d => d).Subscribe(_ => animator.SetBool("dead", true));
+    }
+}
---
+++ EscapeGame/Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class PlayerMove : MonoBehaviour {
+
+    [SerializeField]
+    float movementSpeed;
+
+    [SerializeField]
+    float cameraSpeed;
+
+    [SerializeField]
+    float jumpPower;
+
+    PlayerInputProvider playerInputProvider;
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        playerInputProvider = GetComponent<PlayerInputProvider>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        this.FixedUpdateAsObservable().Subscribe(_ =>
+            rb.AddForce(transform.rotation *playerInputProvider.CharacterMoveDirection.Value * -movementSpeed, ForceMode.Impulse));
+
+        playerInputProvider.CharacterMoveDirection
+            .Where(x => x == Vector3.zero && !playerInputProvider.IsJump.Value).Subscribe(_ => rb.velocity = Vector3.zero);
+
+        playerInputProvider.IsJump.Where(j => j).Subscribe(_ =>
+            rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse));
+
+        playerInputProvider.IsJump.Where(j => !j).Subscribe(_ => rb.velocity = Vector3.zero);
+        
+
+        this.OnCollisionEnterAsObservable().Where(t => t.gameObject.tag == "Ground").Subscribe(_ => playerInputProvider.Land());
+    }
+}
