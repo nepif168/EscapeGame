548141be28f687cbd83d8bbedfb67e46b5699eab
Icchan <nepif168@gmail.com>
2017-09-21 13:52:35 +0900

    設計ミスったので作り直します（interfaceのみ残して）

--- EscapeGame/Assets/Scripts/IInputProvider.cs
+++ EscapeGame/Assets/Scripts/IInputProvider.cs
@@ -8,3 +8,8 @@ public interface IInputProvider
     IReadOnlyReactiveProperty<Vector3> CharacterMoveDirection { get; }
     IReadOnlyReactiveProperty<Vector3> CameraMoveDirection { get; } 
 }
+
+public interface IAdvancedInputProvider : IInputProvider
+{
+    IReadOnlyReactiveProperty<Vector3> DashDirection { get; }
+}
